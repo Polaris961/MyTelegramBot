7955d069bb87055febdb0221b029eae5f8232a2f
aaa <mchlantipov@gmail.com>
2023-08-23 11:43:54 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram. import Command
+from config import settings
+
+
+# Объект бота
+bot = Bot(token=settings.TOKEN)
+
+# Объект диспетчера
+dp = Dispatcher()
+
+
+# Хэндлер на команду старт
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+# Запуск скрипта
+if __name__ == '__main__':
+    # asyncio.run(bot.send_message(1923958618, "Hello from aiogram 3.0.0"))
+    asyncio.run(main())
