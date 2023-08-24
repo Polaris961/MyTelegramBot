9ed208002a5737a7d7c84cfe7de8fe0b4e0600e8
aaa <mchlantipov@gmail.com>
2023-08-24 10:02:57 +0300

    Second commit

---
+++ handlers/_init_.py
@@ -0,0 +1,4 @@
+from .start import dp
+from .any_text import dp
+
+__all__ = ['dp']
--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
+from aiogram import Bot, Dispatcher
+from handlers import start, any_text, sticker
 from config import settings
 
 
-# Объект бота
-bot = Bot(token=settings.TOKEN)
-
-# Объект диспетчера
-dp = Dispatcher()
-
-
-# Хэндлер на команду старт
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
-
-
 async def main():
+    # Объект бота
+    bot = Bot(token=settings.TOKEN)
+    # Объект диспетчера
+    dp = Dispatcher()
+
+    # Добавляем роутеры из хэндлеров в диспетчер
+    dp.include_router(start.router)
+    dp.include_router(sticker.router)
+    dp.include_router(any_text.router)
+
     await dp.start_polling(bot)
 
 
 # Запуск скрипта
 if __name__ == '__main__':
-    # asyncio.run(bot.send_message(1923958618, "Hello from aiogram 3.0.0"))
     asyncio.run(main())
